0af61e4a0b30a104f9accb2849368ace9d60e2eb
Chad Parker <chad@chadparker.com>
2021-04-17 19:52:09 -0700

    Initial Commit

---
+++ HNImporter/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  HNImporter
+//
+//  Created by Chad Parker on 4/17/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
