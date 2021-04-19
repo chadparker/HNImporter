a412b8d0496fac18ab824e8755cb085e11a64e3f
Chad Parker <chad@chadparker.com>
2021-04-18 17:22:20 -0700

    Create basic Post model struct

---
+++ HNImporter/Model/Post.swift
@@ -0,0 +1,31 @@
+//
+//  Post.swift
+//  HNImporter
+//
+//  Created by Chad Parker on 4/17/21.
+//
+
+import Foundation
+
+struct Post {
+
+    let id: String
+
+    let link_text: String
+    let link: String
+    let submitter: String
+
+    let type: String?
+    let source: String?
+    let dead: Bool
+
+    let points: Int?
+    let comments: Int?
+
+    let date: Date
+    let time: Date?
+}
+
+extension Post: Codable {
+    
+}
