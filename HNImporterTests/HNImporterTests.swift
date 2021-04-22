903f5a5c6c0768f187856ac286c59c7c466fe0a8
Chad Parker <chad@chadparker.com>
2021-04-22 14:09:00 -0700

    Add `testDataFilePathsExist()`

--- HNImporterTests/HNImporterTests.swift
+++ HNImporterTests/HNImporterTests.swift
@@ -10,24 +10,9 @@ import XCTest
 
 class HNImporterTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testDataFilePathsExist() {
+        let sut = PostImporter()
+        let filePaths = sut.getDataFilePaths()
+        XCTAssertGreaterThan(filePaths.count, 3000)
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
